f20700c7e6daa9d74de22c7fee7924a473ca858f
Leonardo <sonleo2013@gmail.com>
2020-02-28 00:00:59 -0300

    sms sendo enviado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { BackgroundMode } from '@ionic-native/background-mode/ngx';
+import { SMS } from '@ionic-native/sms/ngx';
 
 @Component({
   selector: 'app-root',
@@ -15,27 +16,34 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private backgroundMode: BackgroundMode
+    private backgroundMode: BackgroundMode,
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-
+      //constante do tempo para segurar o botão (segundos)
       const tempoEspera = 2;
-      var clickInicial = true;
+      //variável booleana para identificar se o clique dado no botão é o primeiro
+      var clickInicial = true; 
+      //variável referente ao segundo que o clique inicial foi dado
       var tempoClickInicial;
 
       window.addEventListener("volumebuttonslistener", onVolumeButtonsListener, false);
 
       function onVolumeButtonsListener() {
         if (clickInicial) {
+          //pegando o tempo em que o clique inicial foi dado
           tempoClickInicial = new Date().getSeconds();
           clickInicial = false;
         }
         else {
           if (new Date().getSeconds() === tempoClickInicial + tempoEspera) {
+            /*
+              entrar na condição somente se o tempo em que o usuário estiver pressionando
+              o botão for  igual ao tempo de espera
+            */
             whenTimeIsReady()
             clickInicial = true;
           }
@@ -43,7 +51,15 @@ export class AppComponent {
       }
 
       function whenTimeIsReady() {
-        alert("Botão pressionado");
+        const sms = new SMS();
+        const num = "993438459";
+        const mensagem = "Estou em perigo, por favor contate a polícia!";
+        const options = {
+          replaceLineBreaks: true
+        }
+        sms.send(num, mensagem, options)
+          .then(() => alert("SMS enviado!"))
+          .catch((err) => alert("Erro ao enviar sms: " + err))
       }
 
       this.backgroundMode.enable();
